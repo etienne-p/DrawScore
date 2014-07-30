5e2057bf279abd5246587e731b5b1d7db7f02fe5
etienne-p <etienne.p.cella@gmail.com>
2014-07-30 11:39:15 -0400

    implement getMidiNotes

--- src/MusicUtils.cpp
+++ src/MusicUtils.cpp
@@ -12,8 +12,19 @@ namespace MusicUtils {
     
     void getMidiNotes(vector<int> &v, Mood mood, int count){
     
-    
-    
+        // http://www.ijpam.eu/contents/2011-67-2/5/5.pdf
+        // http://www.phys.unsw.edu.au/jw/notes.html
+        int major[7] = { 2, 2, 1, 2, 2, 2, 1 };
+        int minor[7] = { 2, 1, 2, 2, 1, 2, 2 };
+        int * scale = mood == MAJOR ? major : minor;
+        
+        int midiNote = 48; // C3
+        v.push_back(midiNote);
+        
+        for (int i = 0; i < count; ++i){
+            midiNote += scale[i % 7];
+            v.push_back(midiNote);
+        }
     }
 
     
